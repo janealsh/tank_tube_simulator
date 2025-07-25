a887c7cc2be8f2fdfb3cfd8061200b84177708e8
Jane Al-Shihabi <janealshihabi@gmail.com>
2025-07-25 14:52:19 -0400

    updates

--- bernoulli.py
+++ bernoulli.py
@@ -5,9 +5,20 @@ import numpy as np
 theta = np.arcsin(1/150);
 theta_deg = theta * (180/np.pi)
 
-# units below in meters
-tank_a = 0.32 * 0.26
+# units m
+a_tank = 0.32 * 0.26
+d_tube = 0.00794
 
-tube_d = 0.00794
+# units m^2
+a_tube = np.square((d_tube/2)) * np.pi
 
-print ("hello", theta_deg)
+# units m/s^2
+g = 9.81
+
+# assuming material is plastic
+
+e_over_d = 0.0015/7.94 
+
+
+
+print ("hello", theta_deg, "a_tube", a_tube)
