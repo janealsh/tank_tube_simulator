de21c1e8d15dee564fc092b3ff6c4912878a8225
janealsh <janealshihabi@gmail.com>
2025-07-25 22:37:34 -0400

    fix while loops a bit

--- bernoulli.py
+++ bernoulli.py
@@ -1,71 +1,85 @@
 import numpy as np
 
-L = 20
+# units m
+L = 0.2
 
-# in kg/m^3
+# units kg/m^3
 rho = 998
 
-# in Ns/m^2
+# units Ns/m^2
 mu = 0.001003
 
-# in m
-epsilon = 0.0000015
+# units m
+# epsilon = 0.0000015
 
-# in degrees
-theta = np.arcsin(1/150)
-theta_deg = theta * (180/np.pi)
+# units degrees
+theta = np.arcsin(1 / 150)
+theta_deg = theta * (180 / np.pi)
 
 # units m
 a_tank = 0.32 * 0.26
 d_tube = 0.00794
 
 # units m^2
-a_tube = np.square((d_tube/2)) * np.pi
+a_tube = ((d_tube / 2) ** 2) * np.pi
 
 # units m/s^2
 g = 9.81
 
-height = 0.08 # units m
+height = 0.08  # units m
 
 stored_v = 0
 
 step = 0.001
 
+# 0.035904566293528936
+
+
 def calculate_vp(height, friction_factor):
-  return np.sqrt((2 * g * (height + L/150)) / (1 - (np.square(a_tube) / np.square(a_tank) + 1.5 + ((L * friction_factor) / d_tube))))
+    pipe_vel_squared = (2 * g * (height + L / 150)) / (2.5 - (a_tube**2 / a_tank**2) + ((L * friction_factor) / d_tube))
+    print("pipe velocity squared is -----------> ", pipe_vel_squared)
+    pipe_velocity = np.sqrt(pipe_vel_squared)
+    return pipe_velocity
+
 
 def calculate_reynolds(velocity):
-  return (rho*velocity*d_tube)/mu # return reynolds number
+    return (rho * velocity * d_tube) / mu  # return reynolds number
+
 
 # assuming material is plastic
-e_over_d = 0.0015/7.94 
+e_over_d = 0.0015 / 7.94
 
-#not working... in progress
+
+# not working... in progress
 def calculate_f(re):
-  colebrook_RHS = -2.0*np.log10((e_over_d/3.7) + (2.51/(re/np.sqrt(f))))
-  return  1/colebrook_RHS**2 # returns f
+    colebrook_RHS = -2.0 * np.log10((e_over_d / 3.7) + (2.51 / (re / np.sqrt(f))))
+    return 1 / colebrook_RHS**2  # returns f
+
 
 drain_time = 0
 
 # iterate while decreasing height
 while height > 0:
-  f_prev = 0.02 
-  f = 0
-#   vp = calculate_vp(height, f_prev)
-#   stored_v = vp
-#   re = calculate_reynolds(stored_v)
-  while abs(f - f_prev) > 0.01:
-    f = f_prev
-    vp = calculate_vp(height, f)
-    re = calculate_reynolds(f)
-    f = calculate_f(re)
-    # if (abs(f - f_prev) < 0.01): 
-    #     break
-    # f_prev = f
+    f_prev = 0.02
+    f = 0
+    print("f_prev (outside loop) --> ", f_prev)
 
-  print("this is f: ", f)
+    while abs(f - f_prev) > 0.01:
+        f = f_prev
+        print("f (in loop) --> ", f)
+        print("HEIGHT (in loop)--> ", height)
 
-  #drain_time += 
-  height -= step
+        vp = calculate_vp(height, f)
+        re = calculate_reynolds(f)
 
-print ("hello", theta_deg, "a_tube", a_tube)
+        f_prev = calculate_f(re)
+        print("new f_prev (in loop) --> ", f_prev)
+
+        print("DIFFERENCE: ", f - f_prev)
+
+    print("this is f: ", f)
+    print(" ")
+
+
+    # drain_time +=
+    height -= step
